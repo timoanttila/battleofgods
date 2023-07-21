785551d481b4184fc6dc43572b34d5e6f79e1510
Timo Anttila <code@tuspe.com>
2023-07-22 02:34:18 +0300

    Breadcrumb fixed, modules updated
    Signed-off-by: Timo Anttila <code@tuspe.com>

--- sveltekit/src/lib/Hero.svelte
+++ sveltekit/src/lib/Hero.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { hero, host, religion, siteName } from '$lib/store'
+  import {page} from '$app/stores'
+  import {hero, host, religion, siteName} from '$lib/store'
 
-	interface BreadcrumbItem {
-    item: string;
-    name: string;
-    position: number;
+  interface BreadcrumbItem {
+    item: string
+    name: string
+    position: number
   }
 
-	$: breadcrumb = <BreadcrumbItem[]|null>null
-	$: imageUrl = `/images/${$hero?.image}`
-	$: metaTitle = $siteName === $hero?.title ? $hero?.title : `${$hero?.title} | ${$siteName}`
-	$: ldjson = <string|null>null
+  $: breadcrumb = <BreadcrumbItem[] | null>null
+  $: imageUrl = `/images/${$hero?.image}`
+  $: metaTitle = $siteName === $hero?.title ? $hero?.title : `${$hero?.title} | ${$siteName}`
+  $: ldjson = <string>''
 
-	$: if ($hero?.pages) {
-		breadcrumb = [
+  $: if ($hero?.pages) {
+    breadcrumb = [
       {
         name: 'Home',
         item: '/',
@@ -25,50 +25,41 @@
         item: page.url,
         position: position + 2
       }))
-    ];
+    ]
 
-		ldjson = `<script type="application/ld+json">${JSON.stringify(breadcrumb)}${'<'}/script>`
-	}
+    ldjson = `<script type="application/ld+json">${JSON.stringify(breadcrumb)}${'<'}/script>`
+  } else {
+    breadcrumb = null
+    ldjson = ''
+  }
 </script>
 
 <svelte:head>
-	<title>{metaTitle}</title>
-	<meta name="twitter:title" property="og:title" content={$hero?.title} />
-	<meta name="description" content={$hero?.description ?? $religion?.description} />
-	<meta name="twitter:description" property="og:description" content={$hero?.description ?? $religion?.description} />
-	<meta name="canonical" property="og:url" content={`${$host}${$page.url.pathname}`} />
-	<meta name="twitter:image" property="og:image" content={`${$host}${imageUrl}-1350.webp`} />
-	{@html ldjson}
+  <title>{metaTitle}</title>
+  <meta name="twitter:title" property="og:title" content={$hero?.title} />
+  <meta name="description" content={$hero?.description ?? $religion?.description} />
+  <meta name="twitter:description" property="og:description" content={$hero?.description ?? $religion?.description} />
+  <meta name="canonical" property="og:url" content={`${$host}${$page.url.pathname}`} />
+  <meta name="twitter:image" property="og:image" content={`${$host}${imageUrl}-1350.webp`} />
+  {@html ldjson}
 </svelte:head>
 
 <div id="hero" class="relative text-center">
-	<img
-		id="hero-image"
-		class="block h-full object-cover w-full"
-		srcset={`${imageUrl}-400.webp 400w, ${imageUrl}-635.webp 635w, ${imageUrl}-1350.webp 1350w`}
-		sizes="(max-width: 500px) 400px, (max-width: 768px) 635px, 1350px"
-		src={`${imageUrl}-1350.webp`}
-		alt={$hero?.alt}
-		width="1350"
-		height="500"
-		decoding="async"
-		loading="eager"
-		aria-hidden="true"
-	/>
-	<div id="hero-content" class="absolute bottom-6 left-0 text-center w-full">
-		<h1 class="leading-tight m-0 mx-auto text-white">{$hero?.title}</h1>
-	</div>
+  <img id="hero-image" class="block h-full object-cover w-full" srcset={`${imageUrl}-400.webp 400w, ${imageUrl}-635.webp 635w, ${imageUrl}-1350.webp 1350w`} sizes="(max-width: 500px) 400px, (max-width: 768px) 635px, 1350px" src={`${imageUrl}-1350.webp`} alt={$hero?.alt} width="1350" height="500" decoding="async" loading="eager" aria-hidden="true" />
+  <div id="hero-content" class="absolute bottom-6 left-0 text-center w-full">
+    <h1 class="leading-tight m-0 mx-auto text-white">{$hero?.title}</h1>
+  </div>
 </div>
 
 {#if Array.isArray(breadcrumb)}
-	<ol id="breadcrumb" class="p-0" vocab="https://schema.org/" typeof="BreadcrumbList">
-		{#each breadcrumb as page (page.item)}
-			<li property="itemListElement" typeof="ListItem">
-				<a href={page.item} property="item" typeof="WebPage">
-					<span property="name">{page.name}</span>
-				</a>
-				<meta property="position" content={String(page.position)} />
-			</li>
-		{/each}
-	</ol>
+  <ol id="breadcrumb" class="p-0" vocab="https://schema.org/" typeof="BreadcrumbList">
+    {#each breadcrumb as page (page.item)}
+      <li property="itemListElement" typeof="ListItem">
+        <a href={page.item} property="item" typeof="WebPage">
+          <span property="name">{page.name}</span>
+        </a>
+        <meta property="position" content={String(page.position)} />
+      </li>
+    {/each}
+  </ol>
 {/if}
--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,130 +1,116 @@
 <script lang="ts">
-  import { hero, heroDefault, religions } from '$lib/store'
-	$hero = {...$heroDefault}
+  import {hero, heroDefault, religions} from '$lib/store'
+  $hero = {...$heroDefault, pages: undefined}
 </script>
 
 <div id="about" class="mb-3 mt-2 mx-auto px text-center">There are many religions in the world, but what is the truth? Who holds the answers? What does history reveal? Let's look for answers together and get to know the teachings, experiences, and historical evidence of the world's largest religions. Open your mind to incredible discoveries and great insights.</div>
 
 <div id="religions" class="gap grid no-underline">
-	{#each $religions as religion}
-		<div id={`religion-${religion.slug}`} class="grid grid-2 religion-content">
-			<div class="religion-image">
-				<img
-					id={`religion-${religion.id}-image`}
-					class="block h-full object-cover w-full"
-					src={`/images/${religion.slug}-635.webp`}
-					alt={`Religion: ${religion.name}`}
-					width="635"
-					height="400"
-					decoding="async" loading="lazy"
-				/>
-			</div>
+  {#each $religions as religion}
+    <div id={`religion-${religion.slug}`} class="grid grid-2 religion-content">
+      <div class="religion-image">
+        <img id={`religion-${religion.id}-image`} class="block h-full object-cover w-full" src={`/images/${religion.slug}-635.webp`} alt={`Religion: ${religion.name}`} width="635" height="400" decoding="async" loading="lazy" />
+      </div>
 
-			<div class="align-center flex religion-info">
-				<div>
-					<h2 class="leading-tight m-0">{religion.name}</h2>
-					<div class="leading religion-summary">{religion.summary}</div>
-					<div>
-						<a
-							class="btn leading-tight rounded"
-							href={`/${religion.slug}/`}
-							title={`What is religion ${religion.name}?`}
-						>
-							Read more &rarr;
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	{/each}
+      <div class="align-center flex religion-info">
+        <div>
+          <h2 class="leading-tight m-0">{religion.name}</h2>
+          <div class="leading religion-summary">{religion.summary}</div>
+          <div>
+            <a class="btn leading-tight rounded" href={`/${religion.slug}/`} title={`What is religion ${religion.name}?`}> Read more &rarr; </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  {/each}
 </div>
 
 <style lang="scss" scoped>
-#religions {
-	.btn {
-		padding: 0.5rem 2rem;
-	}
+  #religions {
+    .btn {
+      padding: 0.5rem 2rem;
+    }
 
-	a:hover {
-		background-color: var(--black);
-		color: var(--white);
-	}
+    a:hover {
+      background-color: var(--black);
+      color: var(--white);
+    }
 
-  h2 {
-    font-size: 2.2rem;
-    line-height: 1.2;
+    h2 {
+      font-size: 2.2rem;
+      line-height: 1.2;
+    }
   }
-}
 
-.religion-image {
-  min-height: 400px;
-}
+  .religion-image {
+    min-height: 400px;
+  }
 
-.religion-summary {
-  margin: 1rem auto 1.5rem;
-}
+  .religion-summary {
+    margin: 1rem auto 1.5rem;
+  }
 
-@media screen and (max-width:1350px) and (min-width:1000px) {
-	#religions {
-		margin: 0 1.5rem;
-	}
-}
+  @media screen and (max-width: 1350px) and (min-width: 1000px) {
+    #religions {
+      margin: 0 1.5rem;
+    }
+  }
 
-@media screen and (min-width:1000px) {
-	#religions {
-		a:not(:hover) {
-			background-color: var(--white);
-			color: var(--primary);
-		}
+  @media screen and (min-width: 1000px) {
+    #religions {
+      a:not(:hover) {
+        background-color: var(--white);
+        color: var(--primary);
+      }
 
-		h2 {
-			color: var(--white);
-		}
-	}
+      h2 {
+        color: var(--white);
+      }
+    }
 
-	.religion-content {
-		background: radial-gradient(79.69% 102.24% at 100% 100.11%, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0)), radial-gradient(89.7% 115.09% at 3.43% 2.75%, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0)), #764ABC;
-		background-blend-mode: overlay, overlay, normal;
-		box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-		padding: 1.5rem;
-		gap: 2rem;
-	}
+    .religion-content {
+      background: radial-gradient(79.69% 102.24% at 100% 100.11%, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0)), radial-gradient(89.7% 115.09% at 3.43% 2.75%, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0)), #764abc;
+      background-blend-mode: overlay, overlay, normal;
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+      padding: 1.5rem;
+      gap: 2rem;
+    }
 
-	.religion-content,
-	.religion-image {
-		border-radius: 20px;
-		overflow: hidden;
-	}
+    .religion-content,
+    .religion-image {
+      border-radius: 20px;
+      overflow: hidden;
+    }
 
-	.religion-summary {
-		color: var(--light);
-	}
-}
+    .religion-summary {
+      color: var(--light);
+    }
+  }
 
-	@media screen and (max-width:1000px) {
-		.religion-content {
-			gap: 1.5rem;
+  @media screen and (max-width: 1000px) {
+    .religion-content {
+      gap: 1.5rem;
 
-			a:not(:hover) {
-				background-color: var(--primary);
-				color: var(--white);
-			}
+      a:not(:hover) {
+        background-color: var(--primary);
+        color: var(--white);
+      }
 
-			h2 {
-				color: var(--primary);
-			}
-		}
+      h2 {
+        color: var(--primary);
+      }
+    }
 
-		.religion-image {
-			height: 400px;
-		}
+    .religion-image {
+      height: 400px;
+    }
 
-		.religion-info {
-			padding: 0 1rem;
-		}
+    .religion-info {
+      padding: 0 1rem;
+    }
 
-		.religion-summary {
-			color: var(--black);
-		}
-	}
+    .religion-summary {
+      color: var(--black);
+    }
+  }
 </style>
